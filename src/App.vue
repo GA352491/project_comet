a8722d4dd00d162c86f1ca521961ae2accf00acb
Anish <GA352491@gmail.com>
2023-12-29 15:01:19 +0530

    modified

--- src/App.vue
+++ src/App.vue
@@ -1,116 +1,154 @@
 <script setup>
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
+import { RouterLink, RouterView } from "vue-router";
+import HelloWorld from "./components/HelloWorld.vue";
 </script>
 
 <template>
   <header>
-
-
-<nav class="navbar navbar-expand-lg bg-body-light">
-  <div class="container-fluid">
-    <a class="navbar-brand" href="#">
-      <img src="../src/assets/logo-3.png" alt="Logo"  class="" style="height: auto;max-width: 100%;width:130px;">
-    </a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse " id="navbarNav">
-      <ul class="navbar-nav  ms-auto mb-2 mb-lg-0 ">
-        <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="#">Become Tutor <span class="tu-tag tu-bggreen">FREE</span></a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link active" href="#">Signup As Student</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link active" href="#">Know More</a>
-        </li>
-        
-    
-      </ul>
-    </div>
-  </div>
-</nav>
+    <nav class="navbar navbar-expand-lg bg-body-light">
+      <div class="container-fluid">
+        <a class="navbar-brand" href="#">
+          <img
+            src="../src/assets/logo-3.png"
+            alt="Logo"
+            class=""
+            style="height: auto; max-width: 100%; width: 130px"
+          />
+        </a>
+        <button
+          class="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarNav"
+          aria-controls="navbarNav"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+          <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+            <li class="nav-item">
+              <a class="nav-link active" aria-current="page" href="#"
+                >Become Tutor <span class="tu-tag tu-bggreen">FREE</span></a
+              >
+            </li>
+            <li class="nav-item">
+              <a class="nav-link active" href="#">Signup As Student</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link active" href="#">Know More</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
   </header>
   <body>
     <RouterView />
-    <div class="container-fluid" style="background-color: #602eaf;color: white;">
-  <footer class="py-5 p-5">
-    <div class="row ">
-      <h5 class="mb-4">Useful links</h5>
-      
-      <div class="col-6 col-md-2 mb-3">
-        <ul class="nav flex-column">
-          <li class="nav-item mb-2"><a href="#" class="nav-link p-0">.  Privacy Policy</a></li>
-          <li class="nav-item mb-2"><a href="#" class="nav-link p-0 ">.  Tutor Signup</a></li>
-      
-        </ul>
-      </div>
+    <div
+      class="container-fluid"
+      style="background-color: #602eaf; color: white"
+    >
+      <footer class="py-5 p-5">
+        <div class="row">
+          <h5 class="mb-4">Useful links</h5>
 
-      <div class="col-6 col-md-2 mb-3">
-     
-        <ul class="nav flex-column ">
-          <li class="nav-item mb-2"><a href="#" class="nav-link p-0 ">.  Top FAQ</a></li>
-          <li class="nav-item mb-2"><a href="#" class="nav-link p-0 ">.  Tutor Login</a></li>
-          
-        </ul>
-      </div>
-
-      <div class="col-6 col-md-2 mb-3">
-    
-        <ul class="nav flex-column">
-          <li class="nav-item mb-2"><a href="#" class="nav-link p-0 ">.  Legal Policies</a></li>
-          <li class="nav-item mb-2"><a href="#" class="nav-link p-0 ">.  Student Signup</a></li>
-         
-        </ul>
-      </div>
-      <div class="col-6 col-md-2 mb-3">
-    
-    <ul class="nav flex-column">
-      <li class="nav-item mb-2"><a href="#" class="nav-link p-0 ">.  TERMS OF USE</a></li>
-      <li class="nav-item mb-2"><a href="#" class="nav-link p-0 ">.  Student login</a></li>
-     
-    </ul>
-  </div>
-      <div class="col-md-3 offset-md-1 mb-3">
-        <form>
-          <h5>Signup for newsletter</h5>
-          <p>Corrupti quolores etmquasa molestias epturite sinteam occaecati amet cupiditate mikume molareshe.</p>
-          <div class="d-flex flex-column flex-sm-row w-100 gap-2">
-            <label for="newsletter1" class="visually-hidden">Email address</label>
-            <input id="newsletter1" type="text" class="form-control" placeholder="Email address">
-            <button class="btn btn-outline-secondary" type="button"><i class="fa-regular fa-paper-plane"></i></button>
+          <div class="col-6 col-md-2 mb-3">
+            <ul class="nav flex-column">
+              <li class="nav-item mb-2">
+                <a href="#" class="nav-link p-0">. Privacy Policy</a>
+              </li>
+              <li class="nav-item mb-2">
+                <a href="#" class="nav-link p-0">. Tutor Signup</a>
+              </li>
+            </ul>
           </div>
-        </form>
-      </div>
 
-    </div>
-     <div class="d-flex flex-column flex-sm-row justify-content-between  mt-5">
-      <p>© 2022 - 2023 All Rights Reserved.</p>
-      <ul class="list-unstyled d-flex">
-        <li class="ms-3"><a class="nav-link" href="#">Privacy Policy</a></li>
-        <li class="ms-3"><a class="nav-link" href="#">Top FAQ</a></li>
-        <li class="ms-3"><a class="nav-link" href="#">Legal Policies</a></li>
-        <li class="ms-3"><a class="nav-link" href="#">TERMS OF USE</a></li>
-      </ul>
-    </div>
-  </footer>
+          <div class="col-6 col-md-2 mb-3">
+            <ul class="nav flex-column">
+              <li class="nav-item mb-2">
+                <a href="#" class="nav-link p-0">. Top FAQ</a>
+              </li>
+              <li class="nav-item mb-2">
+                <a href="#" class="nav-link p-0">. Tutor Login</a>
+              </li>
+            </ul>
+          </div>
+
+          <div class="col-6 col-md-2 mb-3">
+            <ul class="nav flex-column">
+              <li class="nav-item mb-2">
+                <a href="#" class="nav-link p-0">. Legal Policies</a>
+              </li>
+              <li class="nav-item mb-2">
+                <a href="#" class="nav-link p-0">. Student Signup</a>
+              </li>
+            </ul>
+          </div>
+          <div class="col-6 col-md-2 mb-3">
+            <ul class="nav flex-column">
+              <li class="nav-item mb-2">
+                <a href="#" class="nav-link p-0">. TERMS OF USE</a>
+              </li>
+              <li class="nav-item mb-2">
+                <a href="#" class="nav-link p-0">. Student login</a>
+              </li>
+            </ul>
+          </div>
+          <div class="col-md-3 offset-md-1 mb-3">
+            <form>
+              <h5>Signup for newsletter</h5>
+              <p>
+                Corrupti quolores etmquasa molestias epturite sinteam occaecati
+                amet cupiditate mikume molareshe.
+              </p>
+              <div class="d-flex flex-column flex-sm-row w-100 gap-2">
+                <label for="newsletter1" class="visually-hidden"
+                  >Email address</label
+                >
+                <input
+                  id="newsletter1"
+                  type="text"
+                  class="form-control"
+                  placeholder="Email address"
+                />
+                <button class="btn btn-outline-secondary" type="button">
+                  <i class="fa-regular fa-paper-plane"></i>
+                </button>
+              </div>
+            </form>
+          </div>
+        </div>
+        <div
+          class="d-flex flex-column flex-sm-row justify-content-between mt-5"
+        >
+          <p>© 2022 - 2023 All Rights Reserved.</p>
+          <ul class="list-unstyled d-flex">
+            <li class="ms-3">
+              <a class="nav-link" href="#">Privacy Policy</a>
+            </li>
+            <li class="ms-3"><a class="nav-link" href="#">Top FAQ</a></li>
+            <li class="ms-3">
+              <a class="nav-link" href="#">Legal Policies</a>
+            </li>
+            <li class="ms-3"><a class="nav-link" href="#">TERMS OF USE</a></li>
+          </ul>
+        </div>
+      </footer>
     </div>
   </body>
-
-
 </template>
 
 <style scoped>
-body{
-  font-family: 'Helvetica', 
+body {
+  font-family: "Helvetica";
 }
 
 .tu-login-left_title h2 {
-    margin-bottom: 0;
-    color: #ffffff;
-    font-weight: 600;
+  margin-bottom: 0;
+  color: #ffffff;
+  font-weight: 600;
 }
 .nav-link {
   color: #ffffff;
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,108 +1,222 @@
 <template>
-<div class="row">
-    <div class="tu-login-left col-6" >
-                <strong class="pb-5 mb-3">
-                    <a href="index-2.html">
-                      <img src="https://ecomett.com.au/assetsWeb/images/login/logo_white.png" alt="images"></a>
-                </strong>
-                <figure>
-                    <img src="https://ecomett.com.au/assetsWeb/images/login/img-01.png" alt="images">
-                </figure>
-                <div class="tu-login-left_title" style="color:white;">
-                    <h2>Yes! we’re making progress</h2>
-                    <span >every minute &amp; every second</span>
-                </div>
+  <div class="row">
+    <div class="tu-login-left col-6">
+      <strong class="pb-5 mb-3">
+        <a href="index-2.html">
+          <img
+            src="https://ecomett.com.au/assetsWeb/images/login/logo_white.png"
+            alt="images"
+        /></a>
+      </strong>
+      <figure>
+        <img
+          src="https://ecomett.com.au/assetsWeb/images/login/img-01.png"
+          alt="images"
+        />
+      </figure>
+      <div class="tu-login-left_title" style="color: white">
+        <h2>Yes! we’re making progress</h2>
+        <span>every minute &amp; every second</span>
+      </div>
+    </div>
+
+    <div class="col-6 mt-5 pt-5">
+      <div class="d-flex justify-content-center">
+        <div class="tu-login-right_title">
+          <h2 style="color: #602eaf">Welcome!</h2>
+          <h3>It’s really nice to see you</h3>
+        </div>
+      </div>
+
+      <form
+        class="px-5 p-5 mt-5 w-75"
+        style="float: none; margin: auto"
+        @submit.prevent="submitForm"
+      >
+        <div class="mb-3">
+          <input
+            type="text"
+            class="form-control"
+            id="Username"
+            placeholder="Full Name"
+            aria-label="Username"
+            v-model="fullname"
+          />
+        </div>
+        <div class="mb-3">
+          <input
+            type="email"
+            class="form-control"
+            id="exampleInputEmail1"
+            placeholder="Your email address"
+            v-model="email"
+          />
+        </div>
+        <div class="mb-3">
+          <input
+            type="password"
+            class="form-control"
+            placeholder="Enter Password"
+            id="exampleInputPassword1"
+            v-model="password"
+          />
+        </div>
+        <div class="mb-3">
+          <input
+            type="password"
+            class="form-control"
+            placeholder="Re-Enter Password"
+            id="exampleInputPassword2"
+            v-model="password2"
+          />
+        </div>
+        <div class="mb-3">
+          <div class="input-group mb-3">
+            <div class="form-group">
+              <div class="tu-select" style="margin: auto">
+                <select
+                  class="form-control valid"
+                  name="user_timezone"
+                  required=""
+                  aria-required="true"
+                  aria-invalid="false"
+                  v-model="timezone"
+                >
+                  <option value="">Select Timezone</option>
+                  <option value="AWST">Australian Western Standard Time</option>
+                  <option value="ACST">Australian Central Standard Time</option>
+                  <option value="AEST">Australian Eastern Standard Time</option>
+                </select>
+              </div>
+              <p class="text-danger"></p>
             </div>
-
-           <div class="col-6 mt-5 pt-5">
-            <div class="d-flex justify-content-center">
-                <div class="tu-login-right_title" >
-                    <h2 style="color: #602eaf;">Welcome!</h2>
-                    <h3>It’s really nice to see you</h3>
-                </div>
+          </div>
+        </div>
+        <div class="mb-3">
+          <div class="form-group">
+            <div class="tu-check tu-signup-check">
+              <input type="checkbox" id="expcheck2" name="TnC" value="1" />
+              <label for="expcheck2"
+                ><span
+                  >I have read and agree to all
+                  <a href="javascript:void(0);">Terms &amp; conditions</a></span
+                ></label
+              >
             </div>
-            
-<form class="px-5 p-5 mt-5 w-75" style="float:none;margin:auto;">
-  <div class="mb-3">
-    <input type="text" class="form-control" id="Username" placeholder="Full Name" aria-label="Username">
+            <p class="text-danger"></p>
+          </div>
+        </div>
+        <div class="mb-3">
+          <div class="form-group">
+            <div class="form-group">
+              <button
+                type="submit"
+                class="btn btn tu-primbtn-lg w-100"
+                style="background-color: #602eaf; color: white"
+              >
+                <span>Join now</span><i class="icon icon-arrow-right"></i>
+              </button>
+            </div>
+          </div>
+        </div>
+      </form>
+    </div>
   </div>
-  <div class="mb-3">
-    <input type="email" class="form-control" id="exampleInputEmail1" placeholder="Your email address">
-  </div>
-  <div class="mb-3">
-    <input type="password" class="form-control" placeholder="Enter Password" id="exampleInputPassword1">
-  </div>
-  <div class="mb-3">
-    <input type="password" class="form-control" placeholder="Re-Enter Password" id="exampleInputPassword2">
-  </div>
-  <div class="mb-3 ">
-    <div class="input-group mb-3">
-  
-  <div class="form-group">
-    <div class="tu-select" style="margin: auto;">
-                                                    <select class="form-control valid" name="user_timezone" required="" aria-required="true" aria-invalid="false">
-                                                        <option value="">Select Timezone</option>
-                                                        <option value="AWST">
-                                                            Australian Western Standard Time
-                                                        </option>
-                                                        <option value="ACST">
-                                                            Australian Central Standard Time</option>
-                                                        <option value="AEST">
-                                                            Australian Eastern Standard Time</option>
-                                                    </select>
-                                                </div>
-                                                <p class="text-danger"></p>
-
-                                                                                            </div>
-</div>  
-
-</div>
-<div class="mb-3">
-    <div class="form-group">
-                                                <div class="tu-check tu-signup-check">
-                                                    <input type="checkbox" id="expcheck2" name="TnC" value="1">
-                                                    <label for="expcheck2"><span>I have read and agree to all <a href="javascript:void(0);">Terms &amp;
-                                                                conditions</a></span></label>
-                                                </div>
-                                                <p class="text-danger"></p>
-                                                                                            </div>
-</div>
-<div class="mb-3">
-    <div class="form-group">
-                                                <div class="form-group">
-                                                    <button type="submit" class="btn btn tu-primbtn-lg w-100" style="background-color:#602eaf ;color: white;"><span>Join
-                                                            now</span><i class="icon icon-arrow-right"></i></button>
-                                                </div>
-                                            </div>
-</div>
-
-</form>    
-</div> 
-</div>
 </template>
-<script setup>
-// import TheWelcome from '../components/TheWelcome.vue'
-</script>
+
 <style scoped>
 .tu-login-left {
- 
- width: 50%;
- display: flex;
- padding: 50px 30px;
- align-items: center;
- flex-direction: column;
- justify-content: center;
- background-color: #0A0F26;
+  width: 50%;
+  display: flex;
+  padding: 50px 30px;
+  align-items: center;
+  flex-direction: column;
+  justify-content: center;
+  background-color: #0a0f26;
 }
 .tu-login-right {
-    width: 50%;
-    display: flex;
-    padding: 50px 30px;
-    align-items: center;
-    flex-direction: column;
-    justify-content: center;
+  width: 50%;
+  display: flex;
+  padding: 50px 30px;
+  align-items: center;
+  flex-direction: column;
+  justify-content: center;
 }
 .tu-login-right_title {
-    text-align: center;
+  text-align: center;
 }
 </style>
+<script>
+import axios from "axios";
+
+export default {
+  name: "signup",
+  data() {
+    return {
+      fullname: "",
+      email: "",
+      password: "",
+      password2: "",
+      timezone: "",
+      errors: [],
+    };
+  },
+  methods: {
+    submitForm() {
+      console.log(
+        "this is signup form ",
+        this.email,
+        this.fullname,
+        this.password,
+        this.password2
+      );
+      this.errors = [];
+      if (this.fullname == "") {
+        this.errors.push("the username is missing");
+      }
+      if (this.password != this.password2) {
+        this.errors.push("please check password");
+      }
+
+      console.log(this.errors.length, "errors");
+      if (!this.errors.length) {
+        console.log("data enter ");
+        const formData = {
+          email: this.email,
+          fullname: this.fullname,
+          password: this.password,
+          password2: this.password2,
+          timezone: this.timezone,
+          user_type: "tutor",
+        };
+        console.log("formdata", formData);
+        axios
+          .post("http://127.0.0.1:8000/api/signup/", formData)
+          .then((response) => {
+            if (response.status == 201) {
+              const data = response.data;
+              console.log("data", data);
+              this.$router.push("about/");
+            } else {
+              alert(response.data);
+            }
+          })
+          .catch((error) => {
+            if (error.response) {
+              for (const property in error.response.data) {
+                this.errors.push(
+                  `${property}: ${error.response.data[property]}`
+                );
+              }
+              console.log(JSON.stringify(error.response.data));
+            } else if (error.message) {
+              this.errors.push("Something went wrong. Please try again");
+
+              console.log(JSON.stringify(error));
+            }
+          });
+      }
+    },
+  },
+};
+</script>
